b932dfd5f6a237c493cf57213a6b4f7b0a56fdc7
Gabriel Thiago <leucides123@gmail.com>
2025-08-06 21:36:48 -0300

    novas análises adicionadas

--- query.sql
+++ query.sql
@@ -79,3 +79,57 @@ select
 from MoM
 group by months
 order by months
+
+-- for each category which month had highest sales
+with category_sales as (
+select
+	category,
+	format(order_date, 'yyyy-MM') as order_year_month,
+	sum(sale_price) as sales
+from
+	df_orders
+group by category, 
+		 format(order_date, 'yyyy-MM') 
+
+)
+
+select * from (
+select
+	*,
+	ROW_NUMBER() over(partition by category order by sales desc) as rn
+from
+	category_sales
+) a 
+where rn = 1
+
+-- which sub category had highest growth by profit in 2023 compare to 2022
+with sub_cat as (
+select
+	sub_category as subcat, 
+	year(order_date) as order_year,
+	sum(sale_price) as sales
+from
+	df_orders
+group by sub_category,
+		 year(order_date)
+		
+),
+
+sub_cat_two as (
+
+select
+	subcat,
+	sum(case when order_year=2022 then sales else 0 end) as sales_2022,
+	sum(case when order_year=2023 then sales else 0 end) as sales_2023
+from
+	sub_cat
+group by subcat
+)
+
+select top 1
+	*,
+	(sales_2023-sales_2022)*100/sales_2022 as variação
+from
+	sub_cat_two
+order by (sales_2023-sales_2022)*100/sales_2022 desc
+	
